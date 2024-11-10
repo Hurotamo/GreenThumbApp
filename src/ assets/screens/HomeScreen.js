e1b0b20dba4d2ab9c2148e19b9fb4dc4b3810d01
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-10 22:37:20 +0800

    Update HomeScreen.js

--- src/ assets/screens/HomeScreen.js
+++ src/ assets/screens/HomeScreen.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import RewardCard from '../components/RewardCard';
+import { useSelector } from 'react-redux';
+
+const HomeScreen = () => {
+  const userWallet = useSelector((state) => state.user.wallet);
+  const rewards = useSelector((state) => state.activity.rewards);
+
+  return (
+    <View>
+      <Text>Welcome {userWallet ? userWallet.address : 'Guest'}</Text>
+      <Button title="View Wallet" onPress={() => {}} />
+      {rewards.map((reward, index) => (
+        <RewardCard key={index} title={reward.title} points={reward.points} />
+      ))}
+    </View>
+  );
+};
+
+export default HomeScreen;
 
