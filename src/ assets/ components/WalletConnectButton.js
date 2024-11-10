2e72080c1d3f5f7bf369b9af05985a8958e9c863
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-10 22:35:58 +0800

    Update WalletConnectButton.js

--- src/ assets/ components/WalletConnectButton.js
+++ src/ assets/ components/WalletConnectButton.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { Button } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setUserWallet } from '../state/userSlice';
+import { connectWallet } from '../services/solanaService';
+
+const WalletConnectButton = () => {
+  const dispatch = useDispatch();
+
+  const handleConnect = async () => {
+    const wallet = await connectWallet();
+    if (wallet) {
+      dispatch(setUserWallet(wallet));
+    }
+  };
+
+  return <Button title="Connect Wallet" onPress={handleConnect} />;
+};
+
+export default WalletConnectButton;
 
