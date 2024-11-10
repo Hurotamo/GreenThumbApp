90b82106ffa746e5540cf60a888049ce4dc36d56
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-10 22:37:50 +0800

    Update WalletScreen.js

--- src/ assets/screens/WalletScreen.js
+++ src/ assets/screens/WalletScreen.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useSelector } from 'react-redux';
+
+const WalletScreen = () => {
+  const userWallet = useSelector((state) => state.user.wallet);
+
+  return (
+    <View>
+      <Text>Your Wallet Address: {userWallet ? userWallet.address : 'Not connected'}</Text>
+      <Button title="Disconnect Wallet" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default WalletScreen;
 
